6d5ded9e5ffab19a71b5bf3e2b0b0a850ababe4b
Ligh0x74 <1185582263@qq.com>
2024-12-27 11:33:51 +0800

    update: 不使用 cookie 而是使用 token 验证登录

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,14 +1,17 @@
 import axios from 'axios'
 
-// 配置请求, 携带 cookie (用于 session)
+// 配置请求
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 const request = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  withCredentials: true,
 })
 
 // 添加请求拦截器
 request.interceptors.request.use((config) => {
+  const user = localStorage.getItem('user')
+  if (user) {
+    config.headers.Authorization = JSON.parse(user).token
+  }
   return config
 })
 
